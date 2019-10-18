b7957b20ae334ee592986966a3b1fdedf61a9060
lmmLucasMachado <lmlucasmachadom@gmail.com>
2019-10-18 19:25:58 -0300

    concertando erros

--- coin_changing.cpp
+++ coin_changing.cpp
@@ -6,17 +6,16 @@ using namespace std;
 
 int main()  {
     
-    cout << "Coloque o valor recebido" << endl;
+    cout << "Coloque o valor recebido:" << endl;
 
-    float entrada;
-    int buffer = 0;
+    int entrada;
 
     cin >> entrada;
 
-    int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0,c10=0,c11=0,c12=0;
-    // 100 - 50 - 20 - 10 - 5 - 2 - 1 - 0.5 - 0.25 - 0.10 - 0.5 - 0.1
+    int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0;
+    // 100 - 50 - 20 - 10 - 5 - 2 - 1 //
     
-    while(entrada != 0){
+    while(entrada != 0.0){
         if (entrada >=  100){
             entrada = entrada - 100;
             c1++;
@@ -38,23 +37,8 @@ int main()  {
         }else if (entrada >= 1){
             entrada = entrada - 1;
             c7++;
-        }else if (entrada >= 0,5){
-            entrada = entrada - 0.5;
-            c8++;
-        }else if (entrada >= 0.25){
-            entrada = entrada - 0.25;
-            c9++;
-        }else if (entrada >= 0.10){
-            entrada = entrada - 0.10;
-            c10++;
-        }else if (entrada >= 0.5){
-            entrada = entrada - 0.5;
-            c11++;
-        }else if (entrada >= 0.1){
-            entrada = entrada - 0.1;
-            c12++;
-            cout << "oi";
         }
+
     }
 
     cout << "O troco é:" << endl;
@@ -80,21 +64,7 @@ int main()  {
     if (c7 != 0){
         cout << "Numero de 1: " << c7 << endl; 
     }
-    if (c8 != 0){
-        cout << "Numero de 0.5: " << c8 << endl; 
-    }
-    if (c9 != 0){
-        cout << "Numero de 0.25: " << c9 << endl; 
-    }
-    if (c10 != 0){
-        cout << "Numero de 0.10: " << c10 << endl; 
-    }
-    if (c11 != 0){
-        cout << "Numero de 0.5: " << c11 << endl; 
-    }
-    if (c12 != 0){
-        cout << "Numero de 0.1: " << c12 << endl; 
-    }
+
 
     return 0;
 }
